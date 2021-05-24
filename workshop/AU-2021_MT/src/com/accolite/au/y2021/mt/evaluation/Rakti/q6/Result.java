865e62f0081bdef8356a42f1505e138ad0419617
RaktiSingal <rakti1657.cse18@chitkara.edu.in>
2021-05-24 09:36:33 +0530

    Add files via upload

---
+++ workshop/AU-2021_MT/src/com/accolite/au/y2021/mt/evaluation/Rakti/q6/Result.java
@@ -0,0 +1,15 @@
+package package com.accolite.au.y2021.mt.evaluation.Rakti.q6;
+
+import java.util.ArrayList;
+
+public class Result {
+	
+		ArrayList<Horse> list;
+		int count = 0;
+
+		Result() {
+			list = new ArrayList<>();
+		}
+	}
+
+
