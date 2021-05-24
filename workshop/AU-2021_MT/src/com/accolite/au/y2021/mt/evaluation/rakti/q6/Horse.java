9a9674328284e6973a8ca6d9d96b17e7c7382bd7
RaktiSingal <rakti1657.cse18@chitkara.edu.in>
2021-05-24 10:28:19 +0530

    commit

---
+++ workshop/AU-2021_MT/src/com/accolite/au/y2021/mt/evaluation/rakti/q6/Horse.java
@@ -0,0 +1,17 @@
+package package com.accolite.au.y2021.mt.evaluation.Rakti.q6;
+
+public class Horse {
+	
+		int speed = 0,speeds=0,distanceCovered=0,totallSpeedChanges=0;
+		float averageSpeed;
+		int topSpeed = Integer.MIN_VALUE;
+		int lowSpeed = Integer.MAX_VALUE;
+		String speedHistory = "";
+		String name;
+
+		public Horse(String name) {
+			this.name = name;
+
+		}
+	}
+
