1e77fc250da0d18f9779b8fb0870c890258f98ae
Vaibhav Chotani <46738856+VaibhavChotani@users.noreply.github.com>
2021-05-24 18:36:38 +0530

    Update Horse.java

--- workshop/AU-2021_MT/src/com/accolite/au/y2021/mt/evaluation/vaibhav/q6/Horse.java
+++ workshop/AU-2021_MT/src/com/accolite/au/y2021/mt/evaluation/vaibhav/q6/Horse.java
@@ -1,25 +1,22 @@
 package com.accolite.au.y2021.mt.evaluation.vaibhav.q6;
 
-import java.util.concurrent.ThreadLocalRandom;
+import java.util.HashSet;
 
 public class Horse {
-
     String horseName;
 
-	public Horse(String horseName) {
-		this.horseName = horseName;
+    public Horse(String horseName) {
+        this.horseName = horseName;
 
-	}
+    }
 
-
-    int totalDistance = 1000; 
-    int speed = ThreadLocalRandom.current().nextInt(40, 60 + 1); 
-    float timeTaken = 0; 
-    int distanceCovered = 0; 
-    float averageSpeed = 0; 
-    int topSpeed = Integer.MIN_VALUE; 
-    int lowSpeed = Integer.MAX_VALUE; 
-    String speedHistory = ""; 
-    int distanceRemaining; 
-
+    int track_Distance;
+    int distanceCov = 0;
+    int speed = 0;
+    float timeTaken = 0;
+    float averageSpeed = 0;
+    int topSpeed = 0;
+    int lowSpeed = Integer.MAX_VALUE;
+    int distanceRemaining;
+    HashSet<String> history = new HashSet<>(); 
 }
