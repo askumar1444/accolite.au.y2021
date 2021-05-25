015e678d34509bd129a7fcf4e5e061523f88cea0
Vaibhav Chotani <46738856+VaibhavChotani@users.noreply.github.com>
2021-05-25 16:55:33 +0530

    Update Statistics.java

--- workshop/AU-2021_MT/src/com/accolite/au/y2021/mt/evaluation/vaibhav/q6/Statistics.java
+++ workshop/AU-2021_MT/src/com/accolite/au/y2021/mt/evaluation/vaibhav/q6/Statistics.java
@@ -1,40 +1,53 @@
 package com.accolite.au.y2021.mt.evaluation.vaibhav.q6;
 
-
 public class Statistics implements Runnable {
     String horseName;
     Horse reference;
     RandomSpeed random;
 
-    Statistics(Horse reference, String horseName, RandomSpeed random) {
+    Statistics(Horse reference, String horseName) {
         this.reference = reference;
         this.horseName = horseName;
-        this.random = random;
 
     }
-
-    int count = 0;
 
     public void run() {
 
         synchronized (this) {
 
-            if (reference.history.contains(reference.speed + "")) {
-                Thread rs = new Thread();
-                rs.start();
-            } else {
-                reference.history.add(reference.speed + "m/s ");
-            }
+            while (reference.flag != 1) {
 
-            reference.timeTaken += 1;
-            reference.averageSpeed = (float) reference.distanceCov / reference.timeTaken;
+                reference.distanceRemaining = reference.track_Distance - reference.distanceCov;
+                if (reference.distanceRemaining < reference.speed) {
 
-            if (reference.topSpeed < reference.speed) {
-                reference.topSpeed = reference.speed;
-            }
+                    reference.timeTaken += (float) reference.distanceRemaining / reference.speed;
+                    reference.distanceCov = reference.track_Distance;
 
-            if (reference.lowSpeed > reference.speed) {
-                reference.lowSpeed = reference.speed;
+                }
+
+                else {
+
+                    if (reference.history.contains(reference.speed + "")) {
+                        Thread rs = new Thread();
+                        rs.start();
+                    } else {
+
+                        reference.history.add(reference.speed + "m/s ");
+
+                    }
+                    reference.timeTaken += 1;
+                    reference.distanceCov += reference.speed;
+
+                    reference.averageSpeed = (float) reference.track_Distance / reference.timeTaken;
+
+                    if (reference.topSpeed < reference.speed) {
+                        reference.topSpeed = reference.speed;
+                    }
+
+                    if (reference.lowSpeed > reference.speed) {
+                        reference.lowSpeed = reference.speed;
+                    }
+                }
             }
         }
 
