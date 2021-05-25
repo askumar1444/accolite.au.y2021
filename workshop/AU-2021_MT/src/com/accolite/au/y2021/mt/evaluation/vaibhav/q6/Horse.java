d5f50a705a73999d9da5869358e0242c810aca34
Vaibhav Chotani <46738856+VaibhavChotani@users.noreply.github.com>
2021-05-25 16:53:07 +0530

    Update Horse.java

--- workshop/AU-2021_MT/src/com/accolite/au/y2021/mt/evaluation/vaibhav/q6/Horse.java
+++ workshop/AU-2021_MT/src/com/accolite/au/y2021/mt/evaluation/vaibhav/q6/Horse.java
@@ -3,21 +3,23 @@ package com.accolite.au.y2021.mt.evaluation.vaibhav.q6;
 import java.util.HashSet;
 
 public class Horse {
-    String horseName;
 
+    String horseName;
 
     public Horse(String horseName) {
         this.horseName = horseName;
 
     }
-  
+
+    int flag = 0;
     int track_Distance;
     int distanceCov = 0;
     int speed = 0;
     float timeTaken = 0;
     float averageSpeed = 0;
-    int topSpeed = 0;
+    int topSpeed = Integer.MIN_VALUE;
     int lowSpeed = Integer.MAX_VALUE;
     int distanceRemaining;
-    HashSet<String> history = new HashSet<>(); 
+    HashSet<String> history = new HashSet<>();
+
 }
