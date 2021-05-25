38736f61ab40b0ea39fb36337faf076d51bee472
Vaibhav Chotani <46738856+VaibhavChotani@users.noreply.github.com>
2021-05-25 16:54:28 +0530

    Update RandomSpeed.java

--- workshop/AU-2021_MT/src/com/accolite/au/y2021/mt/evaluation/vaibhav/q6/RandomSpeed.java
+++ workshop/AU-2021_MT/src/com/accolite/au/y2021/mt/evaluation/vaibhav/q6/RandomSpeed.java
@@ -1,17 +1,37 @@
 package com.accolite.au.y2021.mt.evaluation.vaibhav.q6;
+
 class RandomSpeed implements Runnable {
     Horse reference;
+    String horseName;
 
-    public RandomSpeed(Horse reference) {
+    public RandomSpeed(Horse reference, String horseName) {
         this.reference = reference;
+        this.horseName = horseName;
     }
 
     int minSpeed = 40;
     int maxSpeed = 60;
     int range = maxSpeed - minSpeed + 1;
+    int count = 0;
 
-    public void run() {
-        reference.speed = (int) (Math.random() * range) + minSpeed;
+    public synchronized void run() {
+
+        System.out.print("Random" + reference.horseName);
+        while (reference.flag != 1 && reference.horseName == horseName) {
+
+            if (count == 0) {
+                reference.speed = (int) (Math.random() * range) + minSpeed;
+
+                count++;
+
+            } else if (reference.timeTaken % 5 == 0) {
+
+                reference.speed = (int) (Math.random() * range) + minSpeed;
+
+            }
+
+        }
 
     }
+
 }
