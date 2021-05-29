57a05ddf58e50f53e5e89961664c893cf3b41ce2
Vaibhav Chotani <46738856+VaibhavChotani@users.noreply.github.com>
2021-05-29 20:57:04 +0530

    Create Counter.java

---
+++ workshop/AU-2021_MT/src/com/accolite/au/y2021/mt/evaluation/vaibhav/q5/Counter.java
@@ -0,0 +1,11 @@
+package com.accolite.au.y2021.mt.evaluation.vaibhav.q5;
+
+public class Counter {
+    int counter1 = 0;
+    int counter2 = 0;
+    int counter3 = 0;
+    boolean isCounting = true;
+    int previousCounterValue2 = 0;
+    int previousCounterValue3 = 0;
+
+}
